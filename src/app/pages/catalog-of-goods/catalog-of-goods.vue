6b92ea03c72845d674ebd77e5f39466c06ff4a3b
Vlad Belikov <v.belikov@aisnovations.com>
2022-07-10 14:38:54 +0300

    added mock of catalog

--- src/app/pages/catalog-of-goods/catalog-of-goods.vue
+++ src/app/pages/catalog-of-goods/catalog-of-goods.vue
@@ -1,7 +1,14 @@
 <template src="./catalog-of-goods.template.html" />
+<style src="./styles.scss" lang="scss" />
+
 <script>
 export default {
-  name: 'CatalogOfGoods'
+  name: 'CatalogOfGoods',
+  data() {
+    return {
+      goods: Array.from({ length: 3})
+    }
+  },
 };
 </script>
 
